f87d5c3072e39aba31bc37cedc53cb9c743db38d
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-03-13 13:47:44 +0300

    Clean up executable class

--- AITaggerCLI/Executable.cs
+++ AITaggerCLI/Executable.cs
@@ -9,41 +9,8 @@ internal static class Executable
 {
     public static int Main(string[] args)
     {
-        Log.Logger = new LoggerConfiguration()
-            #if DEBUG
-            .MinimumLevel.Debug()
-            #endif
-            .WriteTo.Console().CreateLogger();
-        RootCommand rootCommand = new("CLI-tool for AI tags applying.\n" +
-                                      "Original purpose of that app is to allow custom AI models to be used for smart search in Immich. \n" +
-                                      "Requires AITagger REST API endpoint to send your images/videos.");
-
-        Option<string> inputOption = new("--input", "-i")
-        {
-            Description = "Input file (should be video or image)",
-            Required = true
-        };
-        Option<string> endpointOption = new("--endpoint", "-e")
-        {
-            Description = "REST API endpoint, that supports PUT /desc with image uploading.",
-            Required = true
-        };
-        Option<string> xmpFileLocationOption = new("--output", "-o")
-        {
-            Description = "Target file for .xmp files",
-            Required = false
-        };
-        Option<string?> backupOption = new("--backup", "-b")
-        {
-            Description = "Move original file to other location with old_[DATE] prefix.",
-            Required = false,
-            DefaultValueFactory = _ => null
-        };
-
-        rootCommand.Options.Add(inputOption);
-        rootCommand.Options.Add(endpointOption);
-        rootCommand.Options.Add(xmpFileLocationOption);
-        rootCommand.Options.Add(backupOption);
+        SetupLogger();
+        var rootCommand = CreateRootCommand(out var inputOption, out var endpointOption, out var xmpFileLocationOption, out var backupOption);
         rootCommand.SetAction(parseResult =>
         {
             string name = parseResult.GetValue(inputOption)!;
@@ -68,6 +35,51 @@ internal static class Executable
         });
 
         return rootCommand.Parse(args).Invoke();
+    }
+
+    private static void SetupLogger()
+    {
+        Log.Logger = new LoggerConfiguration()
+#if DEBUG
+            .MinimumLevel.Debug()
+#endif
+            .WriteTo.Console().CreateLogger();
+    }
+
+    private static RootCommand CreateRootCommand(out Option<string> inputOption, out Option<string> endpointOption,
+        out Option<string> xmpFileLocationOption, out Option<string?> backupOption)
+    {
+        RootCommand rootCommand = new("CLI-tool for AI tags applying.\n" +
+                                      "Original purpose of that app is to allow custom AI models to be used for smart search in Immich. \n" +
+                                      "Requires AITagger REST API endpoint to send your images/videos.");
+
+        inputOption = new("--input", "-i")
+        {
+            Description = "Input file (should be video or image)",
+            Required = true
+        };
+        endpointOption = new("--endpoint", "-e")
+        {
+            Description = "REST API endpoint, that supports PUT /desc with image uploading.",
+            Required = true
+        };
+        xmpFileLocationOption = new("--output", "-o")
+        {
+            Description = "Target file for .xmp files",
+            Required = false
+        };
+        backupOption = new("--backup", "-b")
+        {
+            Description = "Move original file to other location with old_[DATE] prefix.",
+            Required = false,
+            DefaultValueFactory = _ => null
+        };
+
+        rootCommand.Options.Add(inputOption);
+        rootCommand.Options.Add(endpointOption);
+        rootCommand.Options.Add(xmpFileLocationOption);
+        rootCommand.Options.Add(backupOption);
+        return rootCommand;
     }
 
     private static bool GenerateDescription(IXmpMeta metadata, string name, string endpoint, ParseResult parseResult,
