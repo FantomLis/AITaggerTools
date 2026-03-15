5e84755526005ba68d9530a8a0359ba023f715cc
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-03-15 22:08:54 +0300

    Make Filename in MultiFileException read-only

--- AITaggerCLI/MultiFileException.cs
+++ AITaggerCLI/MultiFileException.cs
@@ -2,7 +2,7 @@
 
 public class MultiFileException : Exception
 {
-    public string Filename;
+    public string Filename { get; init; }
     public MultiFileException(string filename,Exception inner) : base ("File Exception.", inner)
     {
         Filename = filename;
