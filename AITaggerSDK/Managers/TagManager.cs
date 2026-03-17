fa5fb48d8850a02ce5e64292a42d0166c78cc11c
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-03-17 19:05:40 +0300

    Move tag methods to TagManager

--- AITaggerSDK/Managers/TagManager.cs
+++ AITaggerSDK/Managers/TagManager.cs
@@ -1,4 +1,8 @@
-﻿namespace AITaggerSDK.Managers;
+﻿using System.Diagnostics.Contracts;
+using XmpCore;
+using XmpCore.Options;
+
+namespace AITaggerSDK.Managers;
 
 public static class TagManager
 {
@@ -6,4 +10,88 @@ public static class TagManager
     {
         return tagString.Split(", ");
     }
+    
+    public static IXmpMeta ApplyTag(this IXmpMeta xmpMeta, string id, string tag)
+    {
+        xmpMeta.AppendArrayItem(XmpManager.DigikamNs, XmpManager.DigikamTagsList, new PropertyOptions()
+        {
+            IsArray = true
+        }, $"{id}/{tag}", new PropertyOptions());
+        return xmpMeta;
+    }
+
+    public static IXmpMeta ApplyUniqueTag(this IXmpMeta xmpMeta, string id, string tag)
+    {
+        return xmpMeta.ApplyUniqueTags(id, [tag]);
+    }
+
+    private static bool DoesTagListExists(IXmpMeta xmpMeta)
+    {
+        return xmpMeta.DoesPropertyExist(XmpManager.DigikamNs, XmpManager.DigikamTagsList);
+    }
+
+    public static IXmpMeta ClearTags(this IXmpMeta xmpMeta, string id)
+    {
+        if (!DoesTagListExists(xmpMeta)) return xmpMeta;
+        List<int> removedTags = new();
+        for (int i = CountTags(xmpMeta); i >= 1; i--)
+        {
+            var arrVal = GetTag(xmpMeta, i);
+            if (arrVal.Split('/')[0] == id) removedTags.Add(i);
+        }
+        removedTags.ForEach(x => xmpMeta.DeleteArrayItem(XmpManager.DigikamNs, XmpManager.DigikamTagsList, x));
+        return xmpMeta;
+    }
+
+    private static string GetTag(IXmpMeta xmpMeta, int i)
+    {
+        return xmpMeta.GetArrayItem(XmpManager.DigikamNs, XmpManager.DigikamTagsList, i).Value;
+    }
+
+    private static int CountTags(IXmpMeta xmpMeta)
+    {
+        return xmpMeta.CountArrayItems(XmpManager.DigikamNs, XmpManager.DigikamTagsList);
+    }
+
+    public static IXmpMeta ApplyTags (this IXmpMeta xmpMeta, string id, params string[] tags)
+    {
+        foreach (var tag in tags)
+        {
+            xmpMeta.ApplyTag(id, tag);
+        }
+
+        return xmpMeta;
+    }
+    public static IXmpMeta ApplyUniqueTags(this IXmpMeta xmpMeta, string id,  params string[] tags)
+    {
+        if (!DoesTagListExists(xmpMeta)) return ApplyTags(xmpMeta, id, tags);
+        List<string> uniqueTags = tags.ToList();
+        var t = GetAllTaggerTags(xmpMeta, id);
+        for (int i = 0; i < t.Length; i++)
+        {
+            var tagValInMeta = t[i];
+            if (tags.Contains(CleanUpTag(tagValInMeta)))
+                uniqueTags.Remove(CleanUpTag(tagValInMeta));
+        }
+
+        ApplyTags(xmpMeta, id, uniqueTags.ToArray());
+        return xmpMeta;
+    }
+
+    public static string[] GetAllTaggerTags(this IXmpMeta xmpMeta, string id)
+    {
+        if (!DoesTagListExists(xmpMeta)) return [];
+        List<string> tags = new();
+        for (int i = 1; i <= CountTags(xmpMeta); i++)
+        {
+            var arrVal = GetTag(xmpMeta, i);
+            if (arrVal.StartsWith(id+"/")) 
+                tags.Add(arrVal);
+        }
+
+        return tags.ToArray();
+    }
+    
+    [Pure]
+    public static string CleanUpTag(string tag) => tag.Remove(0, tag.IndexOf('/')+1);
 }
--- AITaggerSDK/Managers/XMPManager.cs
+++ AITaggerSDK/Managers/XMPManager.cs
@@ -8,8 +8,8 @@ namespace AITaggerSDK.Managers;
 
 public static class XmpManager
 {
-    const string DigikamNs = "http://www.digikam.org/ns/1.0/";
-    const string DigikamTagsList = "digiKam:TagsList";
+    public const string DigikamNs = "http://www.digikam.org/ns/1.0/";
+    public const string DigikamTagsList = "digiKam:TagsList";
 
     static XmpManager()
     {
@@ -27,87 +27,6 @@ public static class XmpManager
         } else Log.Debug($"File {file} not found. Using clear IXmpMeta.");
         Log.Debug($"Done loading file {file}");
         return xmp;
-    }
-    
-    public static IXmpMeta ApplyTag(this IXmpMeta xmpMeta, string id, string tag)
-    {
-        xmpMeta.AppendArrayItem(DigikamNs, DigikamTagsList, new PropertyOptions()
-        {
-            IsArray = true
-        }, $"{id}/{tag}", new PropertyOptions());
-        return xmpMeta;
-    }
-
-    public static IXmpMeta ApplyUniqueTag(this IXmpMeta xmpMeta, string id, string tag)
-    {
-        return xmpMeta.ApplyUniqueTags(id, [tag]);
-    }
-
-    private static bool DoesTagListExists(IXmpMeta xmpMeta)
-    {
-        return xmpMeta.DoesPropertyExist(DigikamNs, DigikamTagsList);
-    }
-
-    public static IXmpMeta ClearTags(this IXmpMeta xmpMeta, string id)
-    {
-        if (!DoesTagListExists(xmpMeta)) return xmpMeta;
-        List<int> removedTags = new();
-        for (int i = CountTags(xmpMeta); i >= 1; i--)
-        {
-            var arrVal = GetTag(xmpMeta, i);
-            if (arrVal.Split('/')[0] == id) removedTags.Add(i);
-        }
-        removedTags.ForEach(x => xmpMeta.DeleteArrayItem(DigikamNs, DigikamTagsList, x));
-        return xmpMeta;
-    }
-
-    private static string GetTag(IXmpMeta xmpMeta, int i)
-    {
-        return xmpMeta.GetArrayItem(DigikamNs, DigikamTagsList, i).Value;
-    }
-
-    private static int CountTags(IXmpMeta xmpMeta)
-    {
-        return xmpMeta.CountArrayItems(DigikamNs, DigikamTagsList);
-    }
-
-    public static IXmpMeta ApplyTags (this IXmpMeta xmpMeta, string id, params string[] tags)
-    {
-        foreach (var tag in tags)
-        {
-            xmpMeta.ApplyTag(id, tag);
-        }
-
-        return xmpMeta;
-    }
-    public static IXmpMeta ApplyUniqueTags(this IXmpMeta xmpMeta, string id,  params string[] tags)
-    {
-        if (!DoesTagListExists(xmpMeta)) return ApplyTags(xmpMeta, id, tags);
-        List<string> uniqueTags = tags.ToList();
-        var t = GetAllTaggerTags(xmpMeta, id);
-        for (int i = 0; i < t.Length; i++)
-        {
-            var tagValInMeta = t[i];
-            if (tags.Contains(CleanUpTag(tagValInMeta)))
-                uniqueTags.Remove(CleanUpTag(tagValInMeta));
-        }
-
-        ApplyTags(xmpMeta, id, uniqueTags.ToArray());
-        return xmpMeta;
-    }
-
-    public static string[] GetAllTaggerTags(this IXmpMeta xmpMeta, string id)
-    {
-        if (!DoesTagListExists(xmpMeta)) return [];
-        List<string> tags = new();
-        for (int i = 1; i <= CountTags(xmpMeta); i++)
-        {
-            var arrVal = GetTag(xmpMeta, i);
-            if (arrVal.StartsWith(id+"/")) 
-                tags.Add(arrVal);
-        }
-
-        return tags.ToArray();
     }
 
     public static IXmpMeta SaveFile(this IXmpMeta xmpMeta, string file, string? backupPath = null)
@@ -136,9 +55,6 @@ public static class XmpManager
         Log.Debug($"Done saving file {file}.");
         return xmpMeta;
     }
-    
-    [Pure]
-    public static string CleanUpTag(string tag) => tag.Remove(0, tag.IndexOf('/')+1);
 
     [Pure]
     public static string ToXmpFileName(this string filename)
